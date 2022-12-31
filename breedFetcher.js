c673ba68e65f4095ffaa318f3cae1a38329fe6a3
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2022-12-31 16:17:46 -0600

    Test with Mocha and Chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,16 @@
 const request = require('request');
-const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-  };
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log("Breed not found 😿. Try another?")
-    return
-  }
-  console.log(data[0].description);
-  return
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, response.statusCode);
+    }
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      callback(null, ("Breed not found 😿. Try another?"));
+    }
+    callback(null, data[0].description);
+  });
+};
 
-
+module.exports = { fetchBreedDescription };
